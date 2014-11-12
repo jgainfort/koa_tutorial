c755680c0d17752717acee2acea5631cb9eaa95a
jgainfort <gainfortj@gmail.com>
2014-11-12 13:09:36 -0700

    Added request logger middleware

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 var koa = require('koa');
 var app = koa();
 
+// Middleware: request logger
+function *reqLogger(next) {
+    console.log('%s - %s %s',  new Date().toISOString(), this.req.method, this.req.url);
+    yield next;
+}
+app.use(reqLogger);
+
 app.use(function *() {
     this.body = 'hello world!';
 });
