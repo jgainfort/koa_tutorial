b351c1eb5355486512a54d6aafdd764172a0a296
jgainfort <gainfortj@gmail.com>
2014-11-12 22:04:18 -0700

    added ability to render multiple using co-views and swig

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 "use strict"
 
 var Router = require("koa-router");
-var session = require("koa-session");
+var session = require("koa-generic-session");
+var redisStore = require("koa-redis");
+var views = require("co-views");
 var passport = require("./auth");
 
 var koa = require("koa");
 var app = koa();
 
-app.keys = ["?A6Kc!RBWAtWBMBP7QYn&JNV0O"];
+app.keys = ["keys", "keyskeys"];
 app.use(session());
+
+var render = views("templates", {
+    map: {html: "swig"}
+});
 
 // Middleware: request logger
 function *reqLogger(next) {
@@ -19,21 +25,25 @@ app.use(reqLogger);
 
 // Initialize passport
 app.use(passport.initialize());
+app.use(passport.session({
+    cookie: {maxage: 1000 * 60 * 5},
+    store: redisStore()
+}));
 
 var publicRouter = new Router();
 
 // Configure /auth/github and /auth/github/callback
 publicRouter.get("/auth/github", passport.authenticate("github", {scope: ["user", "repo"]}));
-publicRouter.get("/auth/github/callback", passport.authenticate("github", {successRedirect: "/success", failureRedirect: "/"}));
+publicRouter.get("/auth/github/callback", passport.authenticate("github", {successReturnToOrRedirect: "/success", failureRedirect: "/login"}));
 
 publicRouter.get("/auth/facebook", passport.authenticate("facebook"));
-publicRouter.get("/auth/facebook/callback", passport.authenticate("facebook", {successRedirect: "/success", failureRedirect: "/login"}));
+publicRouter.get("/auth/facebook/callback", passport.authenticate("facebook", {successReturnToOrRedirect: "/success", failureRedirect: "/login"}));
 
 publicRouter.get("/auth/google", passport.authenticate("google"));
-publicRouter.get("/auth/google/callback", passport.authenticate("google", {successRedirect: "/success", failureRedirect: "/login"}));
+publicRouter.get("/auth/google/callback", passport.authenticate("google", {successReturnToOrRedirect: "/success", failureRedirect: "/login"}));
 
 publicRouter.get("/auth/twitter", passport.authenticate("twitter"));
-publicRouter.get("/auth/twitter/callback", passport.authenticate("twitter", {successRedirect: "/success", failureRedirect: "/login"}));
+publicRouter.get("/auth/twitter/callback", passport.authenticate("twitter", {successReturnToOrRedirect: "/success", failureRedirect: "/login"}));
 
 app.use(publicRouter.middleware());
 
@@ -43,24 +53,37 @@ var securedRouter = new Router();
 // Middleware: authed for GitHub
 function *authedGithub(next) {
     if (this.req.isAuthenticated()) yield next;
-    else this.redirect("/auth/github");
+    else {
+        //Set redirect path in session
+        this.session.returnTo = this.session.returnTo || this.req.url;
+        this.redirect("/auth/github");
+    }
 }
 
 // Middleware: authed for Facebook
 function *authedFacebook(next) {
     if (this.req.isAuthenticated()) yield next;
-    else this.redirect("/auth/facebook");
+    else {
+        this.session.returnTo = this.session.returnTo || this.req.url;
+        this.redirect("/auth/facebook");
+    }
 }
 
 //Middleware: authed for Google
 function *authedGoogle(next) {
     if (this.req.isAuthenticated()) yield next;
-    else this.redirect("/auth/google");
+    else {
+        this.session.returnTo = this.session.returnTo || this.req.url;
+        this.redirect("/auth/google");
+    }
 }
 
 function *authedTwitter(next) {
     if (this.req.isAuthenticated()) yield next;
-    else this.redirect("/auth/twitter");
+    else {
+        this.session.returnTo = this.session.returnTo || this.req.url;
+        this.redirect("/auth/twitter");
+    }
 }
 
 securedRouter.get("/github", authedGithub, function *() {
@@ -81,21 +104,21 @@ securedRouter.get("/twitter", authedTwitter, function *() {
 
 app.use(securedRouter.middleware());
 
-app.use(publicRouter.middleware());
-
 // Default request
 app.use(function *(next) {
     if (this.path !== "/") return yield next;
 
-    var n = this.session.views || 0;
-    this.session.views = ++n;
-    this.body = "hello world! " + n + " views";
+    var session = this.session;
+    session.count = session.count || 0;
+    session.count++;
+
+    this.body = "hello world!\nCount: " + session.count;
 });
 
 app.use(function *(next) {
     if (this.path !== "/login") return yield next;
 
-    this.body = "Login page (coming soon)";
+    this.body = yield render("login");
 });
 
 app.use(function *(next) {
